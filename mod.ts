fdee59a82c1dd95ee0ea2bccd1285072b2b9f3f7
Adong <51040698@qq.com>
2022-05-03 18:45:29 +0800

    V1.3

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@ export { addYqNum } from './array.ts';
 export { Bmob } from './bmob.ts';
 export { cron, daily } from './cron.ts';
 export { getStrPinYin } from './pinYin.ts';
-export { RSA } from './RSA.ts';
+export { RSA } from './RSA.js';
 export { sha1 } from './sha1.ts';
 export { isChinese, subStrLen, superTrim, trimWenJian, ywbdZzwbd } from './string.ts';
 export { sleep } from './tool.ts';
